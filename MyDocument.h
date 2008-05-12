cb9e5c3d37d28590ef6363692f942d903952e7b8
glaurent@telegraph-road.org <glaurent@telegraph-road.org@c7f880e1-084d-0410-ac0d-6351d5235b9e>
2008-05-12 17:08:42 +0000

    intial import
    git-svn-id: https://edenx.googlecode.com/svn/trunk@2 c7f880e1-084d-0410-ac0d-6351d5235b9e

---
+++ MyDocument.h
@@ -0,0 +1,14 @@
+//
+//  MyDocument.h
+//  orchard
+//
+//  Created by Guillaume Laurent on 4/13/08.
+//  Copyright __MyCompanyName__ 2008 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSPersistentDocument {
+}
+
+@end
